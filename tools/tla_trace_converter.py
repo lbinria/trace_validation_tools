e182b428f78e52fd4df35b620f4d2ca727804fde
lbinria <benjamin.loillier@inria.fr>
2023-04-12 11:24:31 +0200

    Fix variable group by

--- tools/tla_trace_converter.py
+++ tools/tla_trace_converter.py
@@ -10,20 +10,28 @@ def read_trace(filename):
 def merge_dict(d1, d2):
     return d1 | d2
 
+def map_actions(actions):
+    return list(chain(*[map(map_action, actions)]))
+
+def sort_and_group(l, key):
+    # Group by need pre-sorted
+    l.sort(key=key)
+    return groupby(l, key)
+
 def map_action(a):
+#     print(f"Action {a}")
     return { 'op': a['op'], 'path': a['path'], 'args': a['args'] }
 
 def run(input):
     json_trace = read_trace(input)
 
     # Group by clock and sender
+    # TODO sort clock / sender
     json_trace_grouped = [(k, list(g)) for k, g in groupby(json_trace, lambda x: (x['clock'], x['sender']))]
     merged_trace = [list(g) for _, g in sorted(json_trace_grouped, key=lambda x: x[0][0])]
     # Convert into ndjson format expected by TLA+ trace specification
 #     converted_trace = [{event['var']: {'op' : event['op'], 'args' : event['args']} for event in mt} for mt in merged_trace]
-#     converted_trace = [reduce(lambda acc, event: merge_dict(acc, {event['var']: {'op' : event['op'], 'args' : event['args']}}), mt, {}) for mt in merged_trace]
-
-    converted_trace = [{var: list(chain(*[map(map_action, actions)])) for var, actions in groupby(mt, lambda x: x['var'])} for mt in merged_trace]
+    converted_trace = [{var: map_actions(actions) for var, actions in sort_and_group(mt, lambda x: x['var'])} for mt in merged_trace]
 
     # Add config at first line
     json_trace_result = [{"__config": {}}] + converted_trace
