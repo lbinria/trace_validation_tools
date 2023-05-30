f1bcee7c0fe6d279bf3c61bcd61658eb44a6d911
hcirstea <Horatiu.Cirstea@loria.fr>
2023-05-30 20:31:12 +0200

    refactor with lists

---
+++ instrumentation/src/main/java/org/lbee/instrumentation/TraceItem.java
@@ -0,0 +1,26 @@
+package org.lbee.instrumentation;
+
+import java.util.List;
+
+import com.google.gson.JsonObject;
+
+public class TraceItem {
+    private final String action;
+    private List<String> path;
+    private final List<Object> args;
+
+    public TraceItem(String action, List<String> path, List<Object> args) {
+        this.action = action;
+        this.path = path;
+        this.args = args;
+    }
+
+    public JsonObject jsonize() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("op", this.action);
+        jsonObject.add("path", NDJsonSerializer.jsonArrayOf(path));
+        jsonObject.add("args", NDJsonSerializer.serializeValues(args));
+        return jsonObject;
+    }
+    
+}
