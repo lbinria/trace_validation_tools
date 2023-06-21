bfa8e66843afbca188108ed7b657e76477b0e360
lbinria <benjamin.loillier@inria.fr>
2023-06-21 10:46:09 +0200

    Add a function to read json configuration file.

--- instrumentation/src/main/java/org/lbee/instrumentation/ConfigurationWriter.java
+++ instrumentation/src/main/java/org/lbee/instrumentation/ConfigurationWriter.java
@@ -1,10 +1,13 @@
 package org.lbee.instrumentation;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ConfigurationWriter {
@@ -14,6 +17,21 @@ public class ConfigurationWriter {
         final BufferedWriter writer = new BufferedWriter(new FileWriter(path));
         writer.write(jsonObject.toString() + "\n");
         writer.close();
+    }
+
+    public static JsonObject read(String path) throws IOException {
+        final Gson gson = new Gson();
+        final BufferedReader reader = new BufferedReader(new FileReader(path));
+
+        String line = reader.readLine();
+        if (line == null)
+            throw new IOException("Configuration file must contains one json object. Invalid configuration file.");
+
+        final JsonElement jsonLine = JsonParser.parseString(line);
+        if (!jsonLine.isJsonObject())
+            throw new IOException("Configuration file must contains one json object. Invalid configuration file.");
+
+        return jsonLine.getAsJsonObject();
     }
 
 }
