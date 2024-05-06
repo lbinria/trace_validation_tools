55bb47c6e64afac29359fe6ba0268060bc306391
hcirstea <Horatiu.Cirstea@loria.fr>
2024-05-06 10:48:01 +0200

    add comments for helpers for json serialiser

--- src/main/java/org/lbee/instrumentation/helper/NDJsonSerializer.java
+++ src/main/java/org/lbee/instrumentation/helper/NDJsonSerializer.java
@@ -9,22 +9,28 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * A helper class for serializing objects to NDJSON. The class provides methods
+ * for serializing values of basic types, arrays, and maps to JSON elements. It
+ * also accepts objects that implement the {@link TLASerializer} interface.
+ */
 public class NDJsonSerializer {
-
+    /**
+     * Serializes the given values to a JSON array.
+     */
     static JsonElement serializeValues(Object... values) throws IllegalAccessException {
-
         final JsonArray jsonArgs = new JsonArray();
-
         for (Object value : values) {
             jsonArgs.add(serializeValue(value));
         }
-
         return jsonArgs;
     }
 
+    /**
+     * Serializes the given value to a JSON element.
+     */
     static JsonElement serializeValue(Object propertyValue) throws IllegalAccessException {
         final JsonElement jsonValue;
-
         if (propertyValue == null)
             return null;
         else if (propertyValue instanceof String)
@@ -36,61 +42,63 @@ public class NDJsonSerializer {
         else if (propertyValue instanceof Character)
             jsonValue = new JsonPrimitive((Character) propertyValue);
         else if (propertyValue instanceof Enum<?>)
-            jsonValue = new JsonPrimitive(((Enum<?>)propertyValue).ordinal());
+            jsonValue = new JsonPrimitive(((Enum<?>) propertyValue).ordinal());
         else if (propertyValue instanceof Object[])
             jsonValue = jsonArrayOf((Object[]) propertyValue);
         else if (propertyValue instanceof List<?>)
-            jsonValue = jsonArrayOf((List<?>)propertyValue);
+            jsonValue = jsonArrayOf((List<?>) propertyValue);
         else if (propertyValue instanceof HashSet<?>)
-            jsonValue = jsonArrayOf((HashSet<?>)propertyValue);
-        else if (propertyValue instanceof Map<?,?>)
+            jsonValue = jsonArrayOf((HashSet<?>) propertyValue);
+        else if (propertyValue instanceof Map<?, ?>)
             jsonValue = jsonObjectOfMap((Map<?, ?>) propertyValue);
         else if (propertyValue instanceof TLASerializer)
             jsonValue = ((TLASerializer) propertyValue).tlaSerialize();
         else
             throw new IllegalAccessException("Unknown");
-
         return jsonValue;
     }
 
+    /**
+     * Serializes the given list to a JSON array.
+     */
     public static JsonArray jsonArrayOf(List<?> list) throws IllegalAccessException {
         final JsonArray jsonArray = new JsonArray();
-
         for (Object e : list) {
             jsonArray.add(serializeValue(e));
         }
-
         return jsonArray;
     }
 
+    /**
+     * Serializes the given set to a JSON array.
+     */
     public static JsonArray jsonArrayOf(HashSet<?> list) throws IllegalAccessException {
         final JsonArray jsonArray = new JsonArray();
-
         for (Object e : list) {
             jsonArray.add(serializeValue(e));
         }
-
         return jsonArray;
     }
 
+    /**
+     * Serializes the given array to a JSON array.
+     */
     public static JsonArray jsonArrayOf(Object[] array) throws IllegalAccessException {
         final JsonArray jsonArray = new JsonArray();
-
         for (Object e : array) {
             jsonArray.add(serializeValue(e));
         }
-
         return jsonArray;
     }
 
+    /**
+     * Serializes the given map to a JSON object.
+     */
     static JsonObject jsonObjectOfMap(Map<?, ?> map) throws IllegalAccessException {
         final JsonObject jsonObject = new JsonObject();
-
         for (Map.Entry<?, ?> entry : map.entrySet()) {
             jsonObject.add(entry.getKey().toString(), serializeValue(entry.getValue()));
         }
-
         return jsonObject;
     }
-
 }
--- src/main/java/org/lbee/instrumentation/helper/TLASerializer.java
+++ src/main/java/org/lbee/instrumentation/helper/TLASerializer.java
@@ -2,6 +2,9 @@ package org.lbee.instrumentation.helper;
 
 import com.google.gson.JsonElement;
 
+/**
+ * An interface for objects that can be serialized.
+ */
 public interface TLASerializer {
 
     JsonElement tlaSerialize();
